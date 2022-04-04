54514848aa4b246a41566feab5f7caad0a8b2bfd
Michael Chung <mikechung89@gmail.com>
2022-04-04 14:06:29 -0400

    commit tree 2f122f71fca1d7f8d48d1e83bcaf01da624fe0c9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map {|each| "Hello, my name is #{each}."}
+end
+
+def assign_rooms array
+    array.map.with_index {|each, index| "Hello, #{each}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer array
+     batch_badge_creator(array).each {|each| puts "#{each}"}
+     assign_rooms(array).each {|each| puts "#{each}"}
+end
